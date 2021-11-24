393c73daae1ad56794df0b6418c0d4822e3ae193
Yoshi <yoskomu.33@gmail.com>
2021-11-24 20:48:30 +0900

    Indicator の作成

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,10 @@ class BatteryOptimizerPage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: [],
+          children: [
+            OptimizerButtons(),
+            BatteryLevelIndicator(),
+          ],
         ),
       ),
     );
@@ -76,7 +79,7 @@ class _OptimizerButton extends StatelessWidget {
   }
 }
 
-class OptimizerButton extends StatelessWidget {
+class OptimizerButtons extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -95,6 +98,83 @@ class OptimizerButton extends StatelessWidget {
             _OptimizerButton(text: 'Storage Optimizer'),
             SizedBox(width: 16),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _BatteryLevelIndicatorPainter extends CustomPainter {
+  final double percentage;
+  final double textCircleRadius;
+
+  _BatteryLevelIndicatorPainter({
+    required this.percentage,
+    required this.textCircleRadius,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 1; i < (360 * percentage); i += 5) {
+      final per = i / 360.0;
+      final color = ColorTween(
+        begin: kColorIndicatorBegin,
+        end: kColorIndicatorEnd,
+      ).lerp(per)!;
+      final paint = Paint()
+        ..color = color
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 4;
+
+      final spaceLen = 16;
+      final lineLen = 24;
+      final angle = (2 * pi * per) - (pi / 2);
+
+      final offset0 = Offset(size.width * 0.5, size.height * 0.5);
+      final offset1 = offset0.translate(
+          (textCircleRadius + spaceLen) * cos(angle),
+          (textCircleRadius + spaceLen) * sin(angle));
+      final offset2 = offset1.translate(
+        lineLen * cos(angle),
+        lineLen * sin(angle),
+      );
+      canvas.drawLine(offset1, offset2, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class BatteryLevelIndicator extends StatelessWidget {
+  final percentage = 0.7;
+  final size = 164.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _BatteryLevelIndicatorPainter(
+        percentage: percentage,
+        textCircleRadius: size * 0.5,
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(64),
+        child: Material(
+          color: Colors.white,
+          elevation: kElevation,
+          borderRadius: BorderRadius.circular(size * 0.5),
+          child: Container(
+            width: size,
+            height: size,
+            child: Center(
+              child: Text(
+                '${percentage * 100}%',
+                style: TextStyle(color: kColorPink, fontSize: 48),
+              ),
+            ),
+          ),
         ),
       ),
     );
